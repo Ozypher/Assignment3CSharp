275c3c262fe9dc1102928ee1c3475e44508822e0
Sherwin Rahimi <47003261+Ozypher@users.noreply.github.com>
2022-02-10 23:03:26 -0500

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int fiboindex;
+            Console.WriteLine("What number in the sequence are you looking for?");
+            fiboindex = int.Parse(Console.ReadLine());
+            Console.WriteLine(fibonacciroll(fiboindex));
+            
+        }
+        static int fibonacciroll(int index)
+        {
+            if(index <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibonacciroll(index - 1) + fibonacciroll(index - 2);
+            }
+        }
+    }
+}
